70eead7b4950c9fe47951f0a2e2b7325ae9b92e5
User <guglmoj67@gmail.com>
2022-08-26 19:34:10 +0300

    Factorial commit

--- src/Factorial.java
+++ src/Factorial.java
@@ -1,5 +1,17 @@
 import java.util.Scanner;
 
 public class Factorial {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        System.out.println(recursion(n));
+        scanner.close();
+    }
 
+    private static int recursion(int n) {
+        if (n == 1) {
+            return 1;
+        }
+        return n * recursion(n - 1);
+    }
 }
